12ce65ed45f25660efd1e36b3bea3622e6316c8e
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-01-12 18:37:13 -0800

    store url in variable

--- image downloader app/script.js
+++ image downloader app/script.js
@@ -2,3 +2,7 @@ const formEl = document.querySelector('form')
 const inputEl = document.querySelector('#search-input')
 const resultContainer = document.querySelector('.container-results')
 const showMore = document.querySelector('.show-more-button')
+
+const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=0Q0Q5qvefNeLLYncXCRhMGqq47F1lPDML5-We090cf8`
+let inputData = 'school boy';
+let page = 1;
