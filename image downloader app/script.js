11046480659fe752192c5d836707c174a6be173e
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-01-13 15:24:54 -0800

    Update script.js for fetching images

--- image downloader app/script.js
+++ image downloader app/script.js
@@ -1,8 +1,59 @@
 const formEl = document.querySelector('form')
-const inputEl = document.querySelector('#search-input')
+const inputEl = document.getElementById('search')
 const resultContainer = document.querySelector('.container-results')
 const showMore = document.querySelector('.show-more-button')
 
-const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=0Q0Q5qvefNeLLYncXCRhMGqq47F1lPDML5-We090cf8`
-let inputData = 'school boy';
+
+let inputData = '';
 let page = 1;
+
+function searchImage() {
+inputData = inputEl.value;
+const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=0Q0Q5qvefNeLLYncXCRhMGqq47F1lPDML5-We090cf8`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+    console.log(data);
+    const results = data.results;
+
+    if (page === 1) {
+        resultContainer.innerHTML = '';
+    }
+
+    results.map(result => {
+        const imageWrapper = document.createElement('div')
+        imageWrapper.classList.add('result-box')
+        const image = document.createElement('img')
+        image.src = result.urls.small
+        image.alt = result.alt_description
+        const imageLink = document.createElement('a')
+        imageLink.href = result.links.html
+        imageLink.target = '_blank'
+        imageLink.textContent = result.alt_description
+        imageWrapper.appendChild(image)
+        imageWrapper.appendChild(imageLink)
+        resultContainer.appendChild(imageWrapper);
+    });
+
+    if (results.length > 0) {
+        page++;
+        showMore.style.display = 'block';
+    } else {
+        showMore.style.display = 'none';
+    }
+    })
+    .catch(err => {
+        alert(`Error:${err.message}`)
+    })
+}
+
+formEl.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    page = 1;
+    searchImage()
+})
+
+showMore.addEventListener('click', () => {
+searchImage();
+});
