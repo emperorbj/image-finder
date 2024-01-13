f40932de6bbb6fdfc5dc675219da0b2f8e68b7b1
BOLAJI OPATOLA <138551605+emperorbj@users.noreply.github.com>
2024-01-12 18:35:29 -0800

    selected elements

---
+++ image downloader app/script.js
@@ -0,0 +1,4 @@
+const formEl = document.querySelector('form')
+const inputEl = document.querySelector('#search-input')
+const resultContainer = document.querySelector('.container-results')
+const showMore = document.querySelector('.show-more-button')
